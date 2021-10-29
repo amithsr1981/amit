85f2c77b0864de8bc82272d64fa7602c796e5487
Amit Kumar <amit.verma23@gmail.com>
2021-10-29 16:32:14 +0530

    my first code on git

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+region = "ap-south-1"
+}
+resource "aws_instance" "ec2-instance" {
+ami = var.ami_id # referring to ami_id variable
+instance_type = var.instance_type # referring to instance_type variable
+vpc_security_group_ids = [aws_security_group.mysg.id]
+}
+# continued in next slideTERRAFORM MODULE
+resource "aws_security_group" "mysg" {
+name = "allow-ssh"
+description = "Allow ssh traffic"
+vpc_id = var.vpc_id # referring to vpc_id variable
+ingress {
+description = "Allow inbound ssh traffic"
+from_port = var.port
+to_port = var.port
+protocol = "tcp"
+cidr_blocks = [var.cidr_block]
+}
+tags = {
+name = "allow_ssh"
+}
+}
---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "ami_id" {
+default = "ami-041d6256ed0f2061c" #AMI ID specific to Mumbai Region
+}
+variable "instance_type" {
+default = "t2.micro"
+}
+variable "vpc_id" {
+default = "vpc-0ba63d01f2be0c930" #VPC ID of my account's default vpc
+}
+variable "port" {
+default = 22
+}
+variable "cidr_block" {
+default = "0.0.0.0/0"
+}
